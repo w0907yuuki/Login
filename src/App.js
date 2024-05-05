ef54e8a4029cc007785f0bda2d18ae930fdce264
w0907yuuki <w0907yuuki@gmail.com>
2024-05-05 13:29:19 +0900

    front complet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='position'>
+        <h2>システムにログイン</h2>
+        <input type='text' placeholder="IDを入力" className='UserID' id = "txtUserID"></input>
+        <input type='password' placeholder="パスワードを入力" className='Password' id = "txtPassword"></input>
+        <button className='button'>送信</button><br></br>
+        <a href="#" className='link'>アカウントを忘れた場合</a><br></br><br></br>
+        <a className='info'>——————————または——————————</a><br></br>
+        <button className='button'>新しいアカウント作成</button>
+      </div>
     </div>
   );
 }
-
 export default App;
