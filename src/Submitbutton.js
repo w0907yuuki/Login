da405399611f36f6791da3ee43989ab1f6f96169
w0907yuuki <w0907yuuki@gmail.com>
2024-05-06 10:58:36 +0900

    [change] because button express

---
+++ src/Submitbutton.js
@@ -0,0 +1,9 @@
+import App from "./App";
+import React,{ useState } from "react";
+
+const submitbutton = () =>{
+    return(
+        <button className='button'>送信</button>
+    );
+};
+export default submitbutton;
